89b359b5d398e956a65b5d85c5401aa1ff9eb001
MoonSungwoong <46271107+MoonSungwoong@users.noreply.github.com>
2023-07-21 21:16:19 +0900

    Add files via upload

---
+++ study/aws/mysqlsvr.sh
@@ -0,0 +1,16 @@
+rm -rf init-scripts
+
+mkdir init-scripts
+
+cat <<EOF > $(pwd)/init-scripts/init-user.sql
+CREATE USER 'user'@'%' IDENTIFIED WITH mysql_native_password BY 'userpwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user'@'%' WITH GRANT OPTION;
+CREATE DATABASE Test;
+USE Test;
+CREATE TABLE InfoItem (itemId int, itemName varchar(30));
+INSERT INTO InfoItem (itemId, itemName) values (1, 'Apple');
+INSERT INTO InfoItem (itemId, itemName) values (2, 'Orange');
+INSERT INTO InfoItem (itemId, itemName) values (3, 'Grape');
+EOF
+
+docker run -d --rm --name mysqlsvr -e MYSQL_ROOT_PASSWORD=rootpwd -v $(pwd)/init-scripts:/docker-entrypoint-initdb.d -p 3306:3306 mysql:latest
